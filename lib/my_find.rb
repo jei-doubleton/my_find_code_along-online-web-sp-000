52633270734a6469b91f78b0db78f716d50a6bc0
J <jennifer.singleton@gmail.com>
2019-01-15 03:38:59 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,9 +3,7 @@ vc    fvrequire 'pry'
 def my_find(collection)
   i = 0
   while i < collection.length
-    if yield(collection[i])
-      return collection[i]
-    end
+    return collection[i] if yield(collection[i])
     i += 1
   end
 end
